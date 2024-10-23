798a7cd5279c547e6637198c70b4ab23c9f0d1dc
dilgeto <cristobal.torres.u@usach.cl>
2024-10-23 20:42:15 -0300

    Lectura.c/h

--- lab/lab1.c
+++ lab/lab1.c
@@ -1,18 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <getopt.h>
-#include <fitsio.h>
 #include <omp.h>
+#include "parametrizacion.h"
+#include "lectura.h"
 
 int main(int argc, char *argv[]) {
-  /* TODO: Agregar getopt con los parámetros:
-   *   - i: imagen fits
-   *   - a: valor minimo de alpha
-   *   - r: porcentaje aplicado a la circunferencia
-   *   - b: el número de betas
-   *   - u: el número de hebras para el nivel 1
-   *   - d: el número de hebras para el nivel 2
-   */
   int opt;
   char *imagen = NULL;
   double alpha = 0.0;
@@ -44,7 +37,7 @@ int main(int argc, char *argv[]) {
     }
   }
 
-  // Casos de excepción
+  // Casos de fallo
   // TODO: Añadir el caso en que el archivo no exista.
   if (imagen == NULL) {
     printf("Debe incluir una entrada por medio de -i\n");
@@ -73,35 +66,7 @@ int main(int argc, char *argv[]) {
     printf("Incluya la bandera -d y debe ser mayor que 0\n");
     return 0;
   }
-  // TODO: Adaptar a paralelismo en el for
-  // TODO: Cambiar ciclo for por discriminacion entre pixeles borde (255) y pixeles no borde(0)
-  fitsfile *fptr, *gptr; /* pointer to the FITS file; defined in fitsio.h */
-  int status;
-  long fpixel=1, naxis = 2, nelements, exposure;
-  long naxes[2];
- 
-  status = 0; /* initialize status before calling fitsio routines */
- 
-  fits_open_file(&fptr, argv[1], READONLY, &status);
-  
-  fits_get_img_size(fptr, 2, naxes, &status);
-  printf("naxes[0] = %ld\n", naxes[0]);
-  double *myimage = (double *) malloc(naxes[0]*naxes[1]*sizeof(double));
- 
-  printf("size = (%ld, %ld)\n", naxes[1], naxes[0]);
-
-  fits_read_img(fptr, TDOUBLE, fpixel, naxes[0]*naxes[1], NULL, myimage, NULL, &status);
-  printf("%.10e \n", myimage[1024*512+128]);
-  
-  int i, j;
-  double max = -1.0e-6;
-  for (i=0; i < naxes[0]*naxes[1]; i++)
-    if (myimage[i] > max)
-      max = myimage[i];
-      
-  printf("max = %f\n", max);
-
-  // TODO: Creación de lista de puntos bordes (recorrer la imagen para obtener los puntos 255)
+  // TODO: Creación de lista de puntos bordes, llamar a leer_fits
 
   // TODO: Discretización espacio parámetros (algo)
   return 0;
---
+++ lab/lectura.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <sdtlib.h>
+#include <fitsio.h>
+
+// TODO: ARREGLAR ENTRADAS Y SALIDA
+// TODO: Adaptar a paralelismo en el for
+// TODO: Cambiar ciclo for por discriminacion entre pixeles borde (255) y pixeles no borde(0)
+void leer_fits() {
+  fitsfile *fptr, *gptr; /* pointer to the FITS file; defined in fitsio.h */
+  int status;
+  long fpixel=1, naxis = 2, nelements, exposure;
+  long naxes[2];
+
+  status = 0; /* initialize status before calling fitsio routines */
+  fits_open_file(&fptr, argv[1], READONLY, &status);
+  
+  fits_get_img_size(fptr, 2, naxes, &status);
+  printf("naxes[0] = %ld\n", naxes[0]);
+  double *myimage = (double *) malloc(naxes[0]*naxes[1]*sizeof(double));
+
+  printf("size = (%ld, %ld)\n", naxes[1], naxes[0]);
+
+  fits_read_img(fptr, TDOUBLE, fpixel, naxes[0]*naxes[1], NULL, myimage, NULL, &status);
+  printf("%.10e \n", myimage[1024*512+128]);
+  
+  int i, j;
+  double max = -1.0e-6;
+  for (i = 0 ; i < naxes[0]*naxes[1] ; i++) {
+    if (myimage[i] > max)
+      max = myimage[i];
+  }
+  printf("max = %f\n", max);
+  
+}
