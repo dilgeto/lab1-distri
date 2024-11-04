3159da15955df7f74697faf4f151c038dee6cb3d
dilgeto <cristobal.torres.u@usach.cl>
2024-11-03 23:27:47 -0300

    Running (almost)

--- lab/hough.c
+++ lab/hough.c
@@ -73,7 +73,7 @@ Nodo* votacion_paralela(Pixel* pixeles_borde, long ancho, long largo_img, int la
   Nodo* new_elipses = inicializar_lista();
   omp_set_nested(1);
   for (t = 0 ; t < largo ; t++) {
-    #pragma omp parallel num_threads(hebras_1) private(u)
+    #pragma omp parallel num_threads(hebras_1)
     {
       #pragma omp for
         for (u = 0 ; u < largo ; u++) {
@@ -92,7 +92,7 @@ Nodo* votacion_paralela(Pixel* pixeles_borde, long ancho, long largo_img, int la
           }
           theta = calcular_theta(pixeles_borde[t].x, pixeles_borde[t].y, pixeles_borde[u].x, pixeles_borde[u].y);
 
-          #pragma omp parallel num_threads(hebras_2) private(k)
+          #pragma omp parallel num_threads(hebras_2)
           {
             #pragma omp for
               for (k = 0 ; k < largo ; k++) {
@@ -112,28 +112,27 @@ Nodo* votacion_paralela(Pixel* pixeles_borde, long ancho, long largo_img, int la
                 if(comprobacion == 0){
                   continue;
                 }
+                
                 double delta_beta = calcular_delta_beta(largo_img, betas);
                 int beta_discreto = discretizacion(delta_beta, beta);
                 // Se realiza la votación
-                //printf("beta_discreto: %d\n", beta_discreto);
-                //printf("delta_beta: %f\n", delta_beta);
-                voto[beta_discreto]++;
+                #pragma omp critical(votacion)
+                {
+                  voto[beta_discreto]++;
+                }
               }
-            double delta_beta_1 = calcular_delta_beta(largo_img, betas);
-            int max = 0;
-            int index_max = -1;
-            for(int i = 0 ; i < betas ; i++){
-              if(voto[i] < porcentaje*largo || voto[i] <= 0){
-                continue;
-              }
-              if(max < voto[i]){
-                max = voto[i];
-                index_max = i;
-              }
+          }
+          double delta_beta_1 = calcular_delta_beta(largo_img, betas);
+          for(int i = 0 ; i < betas ; i++){
+            if(voto[i] < porcentaje*largo || voto[i] <= 0){
+              continue;
             }
-            double beta_i = (index_max + 1) * delta_beta_1;
+            double beta_i = i * delta_beta_1;
             Elipse* elipse = crear_elipse(oX, oY, alpha, theta, beta_i);
-            new_elipses = agregar_cabeza(new_elipses, elipse);
+            #pragma omp critical(append_elipse)
+            {
+              new_elipses = agregar_cabeza(new_elipses, elipse);
+            }
           }
         }
     }
