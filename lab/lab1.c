35afa977978db63786d04a620b00ece4febdb08f
dilgeto <cristobal.torres.u@usach.cl>
2024-11-04 22:02:11 -0300

    Timers

--- lab/lab1.c
+++ lab/lab1.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <getopt.h>
+#include <time.h>
 #include "parametrizacion.h"
 #include "lectura.h"
 #include "nodo.h"
@@ -64,22 +65,42 @@ int main(int argc, char *argv[]) {
     return 0;
   }
 
+  // Inicio del timer
+  clock_t inicio, fin_lectura, fin_procesado, fin;
+  double tiempo_lectura, tiempo_procesado, tiempo_total;
+
+  inicio = clock();
+
   // TODO: Llamar a leer_fits(Faltan detalles dentro de leer_fits)
   Data* info_img = leer_fits(imagen);
   Pixel* pixeles_borde = info_img->pixeles_borde;
   long ancho = info_img->ancho;
   long largo = info_img->largo;
   int largo_arreglo = info_img->largo_arreglo;
+
+  fin_lectura = clock();
   
   // TODO: Algoritmo de Hough 
   printf("prefuncado\n");
   Nodo* lista_elipses = votacion_paralela(pixeles_borde, ancho, largo, largo_arreglo, u, d, a, r, b);
   printf("funcando\n");
   
+  fin_procesado = clock();
+
   while(lista_elipses->next != NULL){
     printf("%f %f %f %d %f\n", lista_elipses->elipse->o_x,lista_elipses->elipse->o_y,lista_elipses->elipse->alpha,lista_elipses->elipse->beta_i,lista_elipses->elipse->theta);
     lista_elipses = lista_elipses->next;
   }
+
+  fin = clock();
+
+  tiempo_lectura = ((double)(fin_lectura - inicio)) / CLOCKS_PER_SEC;
+  tiempo_procesado = ((double)(fin_procesado - fin_lectura)) / CLOCKS_PER_SEC;
+  tiempo_total = ((double)(fin - inicio)) / CLOCKS_PER_SEC;
+
+  printf("Tiempo lectura:   %f segundos\n", tiempo_lectura);
+  printf("Tiempo procesado: %f segundos\n", tiempo_procesado);
+  printf("Tiempo total:     %f segundos\n", tiempo_total);
 
   return 0;
 }
